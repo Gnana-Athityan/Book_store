a1d957167e85545f81ca6cc06b796fd7e907164c
Gnana Athityan <gnana.athityan@gmail.com>
2024-07-04 11:52:42 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(cors(
         
 app.use(express.json());
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://book-store-frontend-red.vercel.app');
+  res.header('Access-Control-Allow-Origin', 'https://book-store-64qi-frontend.vercel.app');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
