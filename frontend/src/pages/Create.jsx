427cd0701561d6aa6a1e2200e914367a1228852f
Gnana Athityan <125018021@sastra.ac.in>
2024-08-23 22:44:24 +0530

    Update Create.jsx

--- frontend/src/pages/Create.jsx
+++ frontend/src/pages/Create.jsx
@@ -28,7 +28,7 @@ function Create() {
   return (
     <div >
         
-    <Link to = {'/'}><button className='but' style={{textAlign: 'center', marginLeft:'700px', marginBottom:'50px'}}>Home Page</button></Link>
+    <button className='but' style={{textAlign: 'center', marginLeft:'700px', marginBottom:'50px'}}> <Link to = {'/'}>Home Page</Link></button>
     <div className='form1'>
       <label>Title</label><br></br>
         <input type='text' value={Title} onChange={(e)=>settitle(e.target.value)}/><br></br>
